059c0a551061ecfd34ebf3ed326e66749e44098f
Alex <Alex.mclean@bennetts.co.uk>
2023-01-27 11:52:01 +0000

    moves normaliseCwd into common util function file

--- design.bennetts.com/src/utils/commonUtilFunctions.ts
+++ design.bennetts.com/src/utils/commonUtilFunctions.ts
@@ -24,3 +24,7 @@ export const toPascalCase = (string: string): string => {
     .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
     .join("");
 };
+
+export const normaliseCwd = (): string => {
+  return process.cwd().replace(/\\/g, "/");
+};
