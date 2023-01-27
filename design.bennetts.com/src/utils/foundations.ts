37088b7e93bcfc5818a617b2fafb43ca8c50bea5
Alex <Alex.mclean@bennetts.co.uk>
2023-01-27 15:43:56 +0000

    adds generic getStaticProps as a function

---
+++ design.bennetts.com/src/utils/foundations.ts
@@ -0,0 +1,72 @@
+import { GetStaticProps } from "next";
+import path from "path";
+import fs from "fs";
+import { FoundationsProps } from "../components/FoundationsPage/FoundationsPage";
+import { normaliseCwd, uppercaseFirst } from "./commonUtilFunctions";
+import { MarkdownFile } from "../types";
+import { parseMarkdown } from "./markdown";
+import globby from "globby";
+
+const normalisedCwd = normaliseCwd();
+
+export const getStaticPropsForFoundations = (category: string) => {
+  const getStaticProps: GetStaticProps<FoundationsProps> = async () => {
+    const markdownPath = path.posix.join(
+      normalisedCwd,
+      `content/${category}/index.md`
+    );
+    const markdown = fs.readFileSync(markdownPath, "utf-8");
+    const {
+      frontMatter: { description },
+    }: MarkdownFile = parseMarkdown(markdown);
+
+    const globPath = [
+      path.posix.join(normalisedCwd, `content/${category}/*.md`),
+      path.posix.join(normalisedCwd, `content/${category}/*/index.md`),
+    ];
+
+    const itemPaths = globby
+      .sync(globPath)
+      .filter((path) => !path.endsWith(`content/${category}/index.md`));
+
+    let items: FoundationsProps["items"] = [];
+
+    itemPaths
+      .filter((path) => !path.endsWith(`content/${category}/index.md`))
+      .forEach((markdownFilePath) => {
+        const markdown = fs.readFileSync(markdownFilePath, "utf-8");
+        const { frontMatter, readme }: MarkdownFile = parseMarkdown;
+        const {
+          title,
+          description,
+          order,
+          icon,
+          url: frontMatterUrl,
+        } = frontMatter;
+
+        const url =
+          frontMatterUrl ??
+          markdownFilePath
+            .replace(`${normalisedCwd}/content`, "")
+            .replace("/index", "")
+            .replace(/\.md$/, "");
+
+        items.push({
+          title,
+          description: description || "",
+          icon: icon || "",
+          url,
+          order: !isNaN(parseInt(order)) ? order : 1000,
+        });
+      });
+
+    return {
+      props: {
+        title: uppercaseFirst(category),
+        description: description || "",
+        items,
+      },
+    };
+  };
+  return getStaticProps;
+};
